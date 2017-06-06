0ae44b3f8bbf5c73aa9d034b031043965b543a9a
macadmin <macadmin@ITminiSS.local>
2017-06-06 12:00:53 -0500

    Computer validation working.

--- resources/process/existingassets.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$response = false;
-	header("HTTP/1.1 200 OK");
-	header('Content-Type: application/json');
-    echo json_encode($response);
-    return;
-
-?>
---
+++ resources/process/validate.php
@@ -0,0 +1,11 @@
+<?php
+
+	// $response = false;
+// 	header("HTTP/1.1 200 OK");
+// 	header('Content-Type: application/json');
+//     echo json_encode($response);
+//     return;
+
+return true;
+
+?>
